48d16b20e0e06b20be9561b57b212cbe45e8c5fb
Dan Hol <daniel.hollarek@googlemail.com>
2024-04-29 20:43:37 +0200

    Update t_example.py

--- tests/t_example.py
+++ tests/t_example.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 
+# -> For all inspection disabling modifier see (https://gist.github.com/pylover/7870c235867cf22817ac5b096defb768)
 # noinspection PyUnresolvedReferences
 class ExampleTest(TestCase):
     def test_print_hi(self):
